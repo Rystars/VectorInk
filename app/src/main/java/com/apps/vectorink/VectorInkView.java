eb004c0a05787c2078ab69ecf5843d7f33807766
Rystars <96302690+Rystars@users.noreply.github.com>
2025-05-20 09:33:33 +0700

    Update VectorInkView.java

--- app/src/main/java/com/apps/vectorink/VectorInkView.java
+++ app/src/main/java/com/apps/vectorink/VectorInkView.java
@@ -3,6 +3,7 @@ package com.apps.vectorink;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
